f2a762f54d8c21d6edead754095acee2bc3240bc
Harsh Rajput <101476748+Harshalex@users.noreply.github.com>
2025-01-08 16:20:22 +0530

    Update index.js

--- node03/index.js
+++ node03/index.js
@@ -11,7 +11,7 @@ dotenv.config({ path: "./.env" });
 const app = express();
 app.use(cookieparser());
 app.use(express.json({ limit: "10mb" }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://client-side-cyl9.onrender.com", credentials: true }));
 dbConnect();
 
 // Configuration
